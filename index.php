abaf9059bff24b8851a587e86f1a45d5c353da05
sutatip-yamklin <76036775+sutatip-yamklin@users.noreply.github.com>
2020-12-15 16:11:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="th">
+    <head>
+        <meta charset="UTF-8">
+        <title>ทดสอบ HTML</title>
+    </head>
+    <body>
+        <h1>NCT2020</h1>
+        <h2>Resonance</h2>
+
+<p>Ayo, listen up! No matter what they say, no matter what they do We gon' resonate:</p>
+
+<a href="https://www.youtube.com/watch?v=0IpbvXVbBYA">Resonance</a><br>
+        <img src="nct2020.jpg">
+    </body>
+</html>
