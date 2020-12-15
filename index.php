39e649bf61ddc8fdd7b5fedc8582aaa5a79018ea
sutatip-yamklin <76036775+sutatip-yamklin@users.noreply.github.com>
2020-12-15 15:39:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="th">
+    <head>
+        <meta charset="UTF-8">
+        <title>ทดสอบ HTML</title>
+    </head>
+    <body>
+        <h1>NCT2020</h1>
+        <img src="nct2020.jpg">
+    </body>
+</html>
